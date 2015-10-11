13a15b113ff2ecb3a9609cc0394459516aa73e49
Huertix <dhuerta23@gmail.com>
2015-10-12 00:45:39 +0200

    ent
    DB variables

--- web/index.php
+++ web/index.php
@@ -4,8 +4,18 @@ $host = parse_url(getenv("DATABASE_URL"))["host"];
 $database = substr(parse_url(getenv("DATABASE_URL"))["path"], 1);
 $username = parse_url(getenv("DATABASE_URL"))["user"];
 $password = parse_url(getenv("DATABASE_URL"))["pass"];
+$table = "track";
+
+$connection = pg_connect("host=$host port=5432 dbname=$database user=$username password=$password");
+
+$sql = "select * from $table";
+$result = pg_Exec($connection, $sql);
+while($row = pg_fetch_array(($result))){
+	echo $row["id"]."---->LAT: ".$row["latitude"]."----->LNG: ".$row["longitude"];
+	echo "<BR>";
+}
 
 
-echo "hola";
+echo "Hello World!"
 
 ?>

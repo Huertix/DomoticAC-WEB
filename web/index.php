25b4a94ba79aa93799fa5742c264f2ac02bc0055
Huertix <dhuerta23@gmail.com>
2015-10-11 19:41:12 +0200

    DataBase Enviromental

--- web/index.php
+++ web/index.php
@@ -1,5 +1,16 @@
 <?php
 
+$host = parse_url(getenv("DATABASE_URL"))["host"];
+$database = substr(parse_url(getenv("DATABASE_URL"))["path"], 1);
+$username = parse_url(getenv("DATABASE_URL"))["user"];
+$password = parse_url(getenv("DATABASE_URL"))["pass"];
+
+echo $host. "\n";
+echo $database. "\n";
+echo $username. "\n";
+echo $password. "\n";
+
+
 echo "hola";
 
 ?>

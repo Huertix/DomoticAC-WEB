4faf1563bcc29cfb0a29b5c3824f8fc0727ca54a
Huertix <dhuerta23@gmail.com>
2015-10-12 00:30:09 +0200

    ent
    DB variables

--- web/index.php
+++ web/index.php
@@ -5,11 +5,6 @@ $database = substr(parse_url(getenv("DATABASE_URL"))["path"], 1);
 $username = parse_url(getenv("DATABASE_URL"))["user"];
 $password = parse_url(getenv("DATABASE_URL"))["pass"];
 
-echo $host. "\n";
-echo $database. "\n";
-echo $username. "\n";
-echo $password. "\n";
-
 
 echo "hola";
 
